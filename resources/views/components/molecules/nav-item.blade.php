f6ae20bec5fba4646e833582d365c01bdd945f28
ridwanstbd <ridwansetiobudi77@gmail.com>
2026-05-21 05:39:14 +0700

    fix: rombak posisi badge

--- resources/views/components/molecules/nav-item.blade.php
+++ resources/views/components/molecules/nav-item.blade.php
@@ -2,10 +2,11 @@
 
 <a href="{{ $href }}" class="nav-item {{ $active ? 'active' : '' }}" {{ $attributes }}>
     <x-atoms.icon name="{{ $icon }}" class="nav-icon" />
+    
     <span style="flex-grow: 1;">{{ $label }}</span>
     
     @if($badge)
-        <span class="badge" style="background-color: #ef4444; color: white; padding: 2px 6px; border-radius: 12px; font-size: 11px; font-weight: 700; display: inline-flex; align-items: center; justify-content: center; min-width: 20px; height: 20px; z-index: 9999;">
+        <span class="badge" style="background-color: #ef4444; color: white; padding: 2px 6px; border-radius: 12px; font-size: 11px; font-weight: 700; display: inline-flex; align-items: center; justify-content: center; min-width: 20px; height: 20px; margin-right: 16px;">
             {{ $badge }}
         </span>
     @endif
