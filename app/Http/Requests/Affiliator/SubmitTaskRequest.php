a8b5f67526db4d9b822f84c52a20220acf7c596c
ridwanstbd <ridwansetiobudi77@gmail.com>
2026-05-22 10:28:09 +0700

    perbaikan request

--- app/Http/Requests/Affiliator/SubmitTaskRequest.php
+++ app/Http/Requests/Affiliator/SubmitTaskRequest.php
@@ -15,7 +15,7 @@ class SubmitTaskRequest extends FormRequest
     {
         return [
             'tiktok_video_link' => 'required|url|max:1000',
-            'products'          => 'required|array|min:1',
+            'product'          => 'required',
         ];
     }
 
@@ -25,9 +25,7 @@ class SubmitTaskRequest extends FormRequest
             'tiktok_video_link.required' => 'Link video wajib diisi!',
             'tiktok_video_link.url'      => 'Format link tidak valid.',
             'tiktok_video_link.max'      => 'Link video terlalu panjang (maksimal 1000 karakter).',
-            'products.required'          => 'Pilih minimal satu produk yang ditautkan pada video Anda.',
-            'products.array'             => 'Format pilihan produk tidak valid.',
-            'products.min'               => 'Pilih minimal satu produk yang ditautkan pada video Anda.',
+            'product.required'          => 'Pilih minimal satu produk yang ditautkan pada video Anda.',
         ];
     }
 }
