a9c6a84490a73d77e6de88907154259ade99c9e7
ridwanstbd <ridwansetiobudi77@gmail.com>
2026-05-23 14:37:28 +0700

    perbaikannnnnn

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('creator:auto-blacklist')->hourly();
 Schedule::command('app:sync-delivery-status')->everySixHours();
-Schedule::command('product:run-import')->everyMinute()->withoutOverlapping()
-    ->usePhpBinary('/opt/alt/php82/usr/bin/php');
+Schedule::command('product:run-import')->everyMinute()->withoutOverlapping();
